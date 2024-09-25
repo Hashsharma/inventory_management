57bb7e06c292286251ea98c2af4644bb91916afd
scientist <hashsharma0@gmail.com>
2024-09-25 19:09:12 +0530

    "Videos"

--- inventory_mgmt/settings.py
+++ inventory_mgmt/settings.py
@@ -84,10 +84,20 @@ WSGI_APPLICATION = 'inventory_mgmt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': str(BASE_DIR / 'db.sqlite3'),
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'inentory_management',
+        'USER': 'inentory_mgmt',
+        'PASSWORD': 'root@123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
